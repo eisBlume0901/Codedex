7ea2e919964632676a5448e5aae73ae35da2ff1b
eisBlume0901 <anecito.marykate09@gmail.com>
2026-02-19 18:39:26 +0800

    Add script to write and read liked songs to file

---
+++ Python_Intermediate/file_realm.py
@@ -0,0 +1,33 @@
+# Write code below 💖
+
+liked_songs = {
+    "A Million Miles Away": "Kaho Nakamura",
+    "WORK": "millenium parade and Sheena Ringo",
+    "world.execute(me)": "Mili",
+    "One Last Kiss": "Hikaru Utada",
+    "Be a flower": "Ryokuoushoku Shakai",
+    "Again": "Yui"
+}
+
+
+def write_liked_songs_to_file(liked_songs, file_name):
+    try:
+        with open("songs.txt", "w") as file:
+            file.write("Liked Songs:\n")
+            for title, artist in liked_songs.items():
+                file.write(f"{title} by {artist}\n")
+            print(f"Successfully added songs to {file_name}")
+    except IOError:
+        print("Songs not added to file because the file is not found")
+    finally:
+        file.close()
+
+
+write_liked_songs_to_file(liked_songs, "songs.txt")
+
+file = open("songs.txt", "r")
+lines = file.readlines()
+
+for line in lines:
+    print(line, end="")
+
