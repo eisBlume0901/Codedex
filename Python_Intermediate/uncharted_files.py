14465eab4586eb5e80558ecab4bbe724597d8b58
eisBlume0901 <anecito.marykate09@gmail.com>
2026-02-19 18:39:37 +0800

    Add script to write and read secret messages to file

---
+++ Python_Intermediate/uncharted_files.py
@@ -0,0 +1,41 @@
+message = "Hello, there! I am sending you this secret message."
+
+with open("messages.txt", "w") as file:
+    pass
+
+def write_to_file(message: str):
+    try:
+        with open("messages.txt", "a") as file:
+            file.write(message + "\n")
+    except IOError:
+        print("There is an issue writing the file")
+    finally:
+        file.close()
+
+write_to_file(message)
+
+messages = [
+    "Learning Intermediate Python at Codédex",
+    "Learning how to code without AI",
+    "Building fundamental skills rather than becoming a vibe coder"
+]
+
+for i in messages:
+    write_to_file(i)
+
+def read_file(filename: str):
+    try:
+        with open(filename, "r+") as file:
+            lines = file.readlines()
+            for line in lines:
+                print(line, end="")
+    except FileNotFoundError:
+        print("There is an issue reading the file")
+    finally:
+        file.close()
+
+read_file("messages.txt")
+
+# with open("messages.txt", "a") as file:
+#     file.truncate(10)
+
